ef5386f3eb0dfd6384dea32d0fbb95e4169a3acc
Fooserd <fooserd@gmail.com>
2026-03-24 18:45:15 +0100

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 // Cambia 'hotel-wifi' por el nombre real de tu repositorio en GitHub
 export default defineConfig({
-  site: 'https://tu-usuario.github.io',
-  base: '/hotel-wifi',
+  site: 'https://IamYisuSB.github.io',
+  base: '/hotel-cesar-wifi',
 });
